a4b49ef7693e657346c45ffbc7bec9c6426b5df9
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-01-26 09:45:22 +0600

    add exam_type

--- asset/core/prompts.py
+++ asset/core/prompts.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import PromptTemplate
-from asset.core.output_format import out_temp
+from asset.core.output_format import out_temp, DIFFICULTY_MIX_CONFIG
 from langchain.messages import SystemMessage, HumanMessage, AIMessage
 
 """def GenBookPrompt(ex_name, sheet_content, knowledge_content):
@@ -44,13 +44,13 @@ from langchain.messages import SystemMessage, HumanMessage, AIMessage
     return prompt"""
 
 
-def GenQuestionPrompt(ex_name, sheet_content, knowledge_content, n_question):
+def GenQuestionPrompt(ex_name, sheet_content, knowledge_content, n_question, exam_type):
 
     sys_message = SystemMessage(
         content=(
         "You are a professional exam preparation question examiner for the American Petroleum Institute (API).\n"
-        #"Do not need mention API codes or section name or number in the questions But use in explanation if needed."
         f"Your task is to generate {n_question} unique questions based on the provided information.\n\n"
+        f"Exam difficulty mix should be as follows: {DIFFICULTY_MIX_CONFIG[exam_type]}.\n\n"
         f"Follow the specified dictionary output format: {out_temp}.Do not add any extra text except the output list of dictionary template.\n\n"
         f"Do not hellociate or add any extra information outside the specified format. and give {n_question} questions\n"
         )
--- main.py
+++ main.py
@@ -76,7 +76,8 @@ async def config_model(model_name = Form(), temp: float =Form(0.7)):
 async def generate_question(ex_name= Form(), 
                             sheet_content=Form(), 
                             knowledge_content=Form(), 
-                            n_question: int =Form()):
+                            n_question: int =Form(),
+                            exam_type: str = Form()):
     try:
         #prompt = GenQuestionPrompt(ex_name=ex_name, sheet_content=sheet_content, knowledge_content=knowledge_content, n_question=n_question)
         
@@ -87,14 +88,16 @@ async def generate_question(ex_name= Form(),
         status, text = GetModelResponse(model=model, ex_name=ex_name,
                                     sheet_content=sheet_content,
                                    knowledge_content=knowledge_content,
-                                   n_question=n_question)
+                                   n_question=n_question,
+                                   exam_type=exam_type)
         s_count = 0
         while not status and s_count < 3:
             print(f"Retrying... Attempt {s_count + 1}")
             status, text = GetModelResponse(model=model, ex_name=ex_name,
                                         sheet_content=sheet_content,
                                        knowledge_content=knowledge_content,
-                                       n_question=n_question)
+                                       n_question=n_question,
+                                       exam_type=exam_type)
             s_count += 1
         
         s_count = 0
@@ -105,7 +108,8 @@ async def generate_question(ex_name= Form(),
             new_data = GetModelResponse(model=model, ex_name=ex_name,
                                         sheet_content=sheet_content,
                                         knowledge_content=knowledge_content,
-                                        n_question=count)
+                                        n_question=count,
+                                        exam_type=exam_type)
             
             response = MergeData(previous=text, new=new_data)
 
